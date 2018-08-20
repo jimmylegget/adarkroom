8448c5f8c35d72a06799a35e546e90dd07112991
jim <34107597+jimmylegget@users.noreply.github.com>
2018-08-20 00:12:55 -0400

    Update engine.js

--- script/engine.js
+++ script/engine.js
@@ -17,57 +17,7 @@
 				/// TRANSLATORS : means with more force.
 				notify: _('learned to throw punches with purpose')
 			},
-			'martial artist': {
-				name: _('martial artist'),
-				desc: _('punches do even more damage.'),
-				notify: _('learned to fight quite effectively without weapons')
-			},
-			'unarmed master': {
-				/// TRANSLATORS : master of unarmed combat
-				name: _('unarmed master'),
-				desc: _('punch twice as fast, and with even more force'),
-				notify: _('learned to strike faster without weapons')
-			},
-			'barbarian': {
-				name: _('barbarian'),
-				desc: _('melee weapons deal more damage'),
-				notify: _('learned to swing weapons with force')
-			},
-			'slow metabolism': {
-				name: _('slow metabolism'),
-				desc: _('go twice as far without eating'),
-				notify: _('learned how to ignore the hunger')
-			},
-			'desert rat': {
-				name: _('desert rat'),
-				desc: _('go twice as far without drinking'),
-				notify: _('learned to love the dry air')
-			},
-			'evasive': {
-				name: _('evasive'),
-				desc: _('dodge attacks more effectively'),
-				notify: _("learned to be where they're not")
-			},
-			'precise': {
-				name: _('precise'),
-				desc: _('land blows more often'),
-				notify: _('learned to predict their movement')
-			},
-			'scout': {
-				name: _('scout'),
-				desc: _('see farther'),
-				notify: _('learned to look ahead')
-			},
-			'stealthy': {
-				name: _('stealthy'),
-				desc: _('better avoid conflict in the wild'),
-				notify: _('learned how not to be seen')
-			},
-			'gastronome': {
-				name: _('gastronome'),
-				desc: _('restore more health when eating'),
-				notify: _('learned to make the most of food')
-			}
+			
 		},
 
 		options: {
@@ -434,54 +384,7 @@
 			});
 		},
 
-		share: function() {
-			Events.startEvent({
-				title: _('Share'),
-				scenes: {
-					start: {
-						text: [_('bring your friends.')],
-						buttons: {
-							'facebook': {
-								text: _('facebook'),
-								nextScene: 'end',
-								onChoose: function() {
-									window.open('https://www.facebook.com/sharer/sharer.php?u=' + Engine.SITE_URL, 'sharer', 'width=626,height=436,location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no');
-								}
-							},
-							'google': {
-								text:_('google+'),
-								nextScene: 'end',
-								onChoose: function() {
-									window.open('https://plus.google.com/share?url=' + Engine.SITE_URL, 'sharer', 'width=480,height=436,location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no');
-								}
-							},
-							'twitter': {
-								text: _('twitter'),
-								nextScene: 'end',
-								onChoose: function() {
-									window.open('https://twitter.com/intent/tweet?text=A%20Dark%20Room&url=' + Engine.SITE_URL, 'sharer', 'width=660,height=260,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
-								}
-							},
-							'reddit': {
-								text: _('reddit'),
-								nextScene: 'end',
-								onChoose: function() {
-									window.open('http://www.reddit.com/submit?url=' + Engine.SITE_URL, 'sharer', 'width=960,height=700,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
-								}
-							},
-							'close': {
-								text: _('close'),
-								nextScene: 'end'
-							}
-						}
-					}
-				}
-			},
-			{
-				width: '400px'
-			});
-		},
-
+		
 		findStylesheet: function(title) {
 			for(var i=0; i<document.styleSheets.length; i++) {
 				var sheet = document.styleSheets[i];
